c13dc2b28adeb971af53527c64a63301c281d99d
Zakariae <zakariaezafzafi@gmail.com>
2025-11-24 17:35:29 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export function loadConfigFromCli(): AppConfig {
     .option(
       "--reconcile-interval <ms>",
       "Reconciliation interval in milliseconds (0 = disabled)",
-      "30000"
+      "60000"
     );
 
   program.parse(process.argv);
